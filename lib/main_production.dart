02f16dcb2387e6c4daff7766dfc2fdebfbd6d156
AhmedYasserabdelraziq <ahmedyaserramadan456@gmail.com>
2024-06-02 01:17:00 +0300

    -added fastlane to android and connected it to firebase distribution. -Bumped version 1.0.0 ✅

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -7,7 +7,6 @@ import 'package:book_shop/services/services_locator.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
